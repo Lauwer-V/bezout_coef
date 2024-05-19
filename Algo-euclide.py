f2d3fe3d675fac2cc35c0e0dd54fc116d6214333
Lauwer-V <73760114+Lauwer-V@users.noreply.github.com>
2024-05-20 01:16:44 +0200

    Update Algo-euclide.py
    Ajout de commentaire et changement des noms de variables

--- Algo-euclide.py
+++ Algo-euclide.py
@@ -1,61 +1,64 @@
-def PGCD(a,b, l={}):
-    def get_k(a,b,r):
-        s=0
-        while a!=(b*s+r):
-            s += 1
-        return s
+# Fonction pour calculer le PGCD de deux nombres et les coefficients de Bézout intermédiaires
+def pgcd_bezout(a, b, coefficients={}):
+    # Fonction auxiliaire pour calculer le coefficient k tel que a = b*k + r
+    def get_coeff(a, b, r):
+        k = 0
+        while a != (b * k + r):
+            k += 1
+        return k
 
-    if a%b == 0:
-        return l
+    # Si a est divisible par b, renvoyer le dictionnaire des coefficients de Bézout
+    if a % b == 0:
+        return coefficients
     else:
-        l[a%b] = [(a,1),(b,-get_k(a,b,a%b))]
-        return PGCD(b,a%b,l)
+        # Mettre à jour le dictionnaire des coefficients de Bézout
+        coefficients[a % b] = [(a, 1), (b, -get_coeff(a, b, a % b))]
+        # Appel récursif de la fonction pgcd_bezout avec les nouveaux arguments b et a % b
+        return pgcd_bezout(b, a % b, coefficients)
 
-def coef_bezout(a,b):
-    def elt_etranger_in_egalite(a,b,s):
-        retour = False
+# Fonction pour calculer les coefficients de Bézout de deux nombres
+def bezout_coefficients(a, b):
+    # Fonction auxiliaire pour vérifier si l'égalité de Bézout contient des éléments autres que a et b
+    def has_other_elt(a, b, s):
         for elt in s:
-            #print(elt[0])
-            if (elt[0] != a) and (elt[0] != b):
-                retour = True
-        return retour
-    
-    def mult(old):
-        coef = old[1]
-        new = descent[s[i][0]]
-        return [(new[0][0],new[0][1]*coef),(new[1][0],new[1][1]*coef)]
+            if elt[0] not in (a, b):
+                return True
+        return False
 
-    def somme(l):
-        submit = [0,0]
+    # Fonction auxiliaire pour multiplier un coefficient de Bézout par un autre
+    def mult_coeff(old):
+        coeff = old[1]
+        new_coeff = coefficients[s[i][0]]
+        return [(new_coeff[0][0], new_coeff[0][1] * coeff),
+                (new_coeff[1][0], new_coeff[1][1] * coeff)]
+
+    # Fonction auxiliaire pour calculer la somme des coefficients de Bézout
+    def sum_coeff(l):
+        a_coeff, b_coeff = 0, 0
         for elt in l:
             if elt[0] == a:
-                submit[0] = submit[0]+elt[1]
-            if elt[0] == b:
-                submit[1] = submit[1]+elt[1]
+                a_coeff += elt[1]
+            elif elt[0] == b:
+                b_coeff += elt[1]
+        print(f"L'égalité de Bezout : {a} ⨯ {a_coeff} + {b} ⨯ {b_coeff} = {gcd}")
+        return a_coeff, b_coeff
 
-        print(f"L'égalité de Bezout : {a} ⨯ {submit[0]} + {b} ⨯ {submit[1]} = {gcd}")
-        return submit
+    # Calculer le PGCD et les coefficients de Bézout intermédiaires
+    global coefficients
+    coefficients = pgcd_bezout(a, b)
+    # Récupérer le PGCD
+    gcd = min(coefficients.keys())
+    # Initialiser la liste des coefficients de Bézout
+    s = [(gcd, 1)]
 
-    descent = PGCD(a,b)
-    gcd = min(descent.keys())
-    s=[(gcd,1)]
-    
-    while elt_etranger_in_egalite(a,b,s):
+    # Tant que l'égalité de Bézout contient des éléments autres que a et b
+    while has_other_elt(a, b, s):
         for i in range(len(s)):
-            if (s[i][0] != a) or (s[i][0] != b):
-                if s[i][0] in descent:
-                    add = mult(s[i]) #descent[s[i][0]]
-                    s.pop(i)
-                    s.append(add[0])
-                    s.append(add[1])
+            if s[i][0] not in (a, b) and s[i][0] in coefficients:
+                add_coeff = mult_coeff(s[i])
+                s.pop(i)
+                s.append(add_coeff[0])
+                s.append(add_coeff[1])
 
-    return somme(s)
-
-        
-            
-            
-            
-
-
-gcd = coef_bezout(537,123)
-print(gcd)
+    # Calculer et renvoyer les coefficients de Bézout
+    return sum_coeff(s)
